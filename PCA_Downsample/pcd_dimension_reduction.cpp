372213feb25dd8a015cc90fda6ea4fa2b03d063d
qinziwen <qinziwen@baidu.com>
2020-04-16 10:32:22 +0800

    Reconstruct pointcloud after reduction

--- PCA_Downsample/pcd_dimension_reduction.cpp
+++ PCA_Downsample/pcd_dimension_reduction.cpp
@@ -17,17 +17,21 @@ int main(int argc, char** argv) {
     const Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> eigen_vector = pca.get_eigen_vector();
 
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> eigen_1 = pcd.transpose() * eigen_vector.col(0);
-    if (!save_pointcloud_to_file(eigen_1, "eigen_1.txt")) {
+    // Reconstruct, x^T * z = a  --> x^T = a * z^T
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> recons_1 = eigen_1 * eigen_vector.col(0).transpose();
+    if (!save_pointcloud_to_file(recons_1, "eigen_1.txt")) {
         std::cerr << "save_pointcloud_to_file failed" << std::endl;
         return 1;
     }
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> eigen_2 = pcd.transpose() * eigen_vector.leftCols(2);
-    if (!save_pointcloud_to_file(eigen_2, "eigen_2.txt")) {
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> recons_2 = eigen_2 * eigen_vector.leftCols(2).transpose();
+    if (!save_pointcloud_to_file(recons_2, "eigen_2.txt")) {
         std::cerr << "save_pointcloud_to_file failed" << std::endl;
         return 1;
     }
 
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> eigen_3 = pcd.transpose() * eigen_vector;
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> recons_3 = eigen_3 * eigen_vector.transpose();
     if (!save_pointcloud_to_file(eigen_3, "eigen_3.txt")) {
         std::cerr << "save_pointcloud_to_file failed" << std::endl;
         return 1;
